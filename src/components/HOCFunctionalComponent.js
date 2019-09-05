0457079204d44b25b55bfc640ec127d848449108
Shourya Mangal <smangal@sgssupport.abudhabi.ae>
2019-09-05 12:52:24 +0400

    add profiler to measure component performance

--- src/components/HOCFunctionalComponent.js
+++ src/components/HOCFunctionalComponent.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import {Profiler} from 'react';
 import Loading from "./HOC/Loading";
 
 function HOCFunctionalComponent(props) {
   const greeting = 'High order Function Component!';
   const { name, contacts } = props;
-  return (<React.Fragment>
-    <h1>{greeting}</h1>
-    <strong>Welcome {name} </strong>
-    <p>Contacts length: {contacts ? contacts.length : 0} entries</p>
-    <hr />
-  </React.Fragment>);
+
+  return (
+    <Profiler id="HOCFunctionalComponent" onRender={onRenderCallback}>
+      <React.Fragment>
+        <h1>{greeting}</h1>
+        <strong>Welcome {name} </strong>
+        <p>Contacts length: {contacts ? contacts.length : 0} entries</p>
+        <hr />
+      </React.Fragment>
+    </Profiler>
+  );
+  function onRenderCallback(
+    id, // the "id" prop of the Profiler tree that has just committed
+    phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+    actualDuration, // time spent rendering the committed update
+    baseDuration, // estimated time to render the entire subtree without memoization
+    startTime, // when React began rendering this update
+    commitTime, // when React committed this update
+    interactions // the Set of interactions belonging to this update
+  ) {
+
+    console.log({"id ":id,"phase ":phase," actualDuration ":actualDuration,"baseDuration ":baseDuration," startTime ":startTime," commitTime ":commitTime,"interactions  ":interactions});
+  }
 }
 
 export default Loading("contacts")(HOCFunctionalComponent);
